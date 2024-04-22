1ce4296091cd4e6e81fa85d1e36da0fba1622ffc
Aasif khan <akay41024@gmail.com>
2024-04-22 23:18:11 +0530

    fibonnaci in c++

---
+++ fibonacci.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int fibonacci(int n){
+    int a = 0, b = 1, c;
+    if(n == 0){
+        cout << a << " ";
+        return a;
+    }
+    cout << a << " " << b << " ";
+    for(int i = 2; i < n; i++){
+        c = a + b;
+        cout << c << " ";
+        a = b;
+        b = c;
+    }
+    return c;
+}
+int main()
+{
+    int n;
+    cout <<"Enter the number of terms in the febonacci series";
+    cin >> n;
+    cout <<"Fibonacci series : ";
+    fibonacci(n);
+    return 0;
+}
