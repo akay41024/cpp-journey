b999bbf688274c52240d7a7dec2e3f4d575a4eb1
Aasif khan <akay41024@gmail.com>
2024-09-20 23:12:32 +0530

    Add array.cpp for dynamic array input and output

---
+++ array/array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int  n;
+    cin >> n;
+    // int arr[n] = {1,3, 44, 5}; we can do that  but it is not recommended in c++
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    for(int x:arr){
+        cout << x << endl;
+    }
+
+    return 0;
+}
